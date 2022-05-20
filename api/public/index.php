7935f86e7f85a9e948cac92f6fc1a8d05f455fb5
Alexandr Bubeshko <a.bubeshko@gmail.com>
2022-05-20 17:16:47 +0300

    build: add phpunit

--- api/public/index.php
+++ api/public/index.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\Kernel;
 
 require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/config/bootstrap.php';
 
 /**
  * @param array{APP_ENV: string, APP_DEBUG: int} $context
---
+++ api/tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+declare(strict_types=1);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+require_once dirname(__DIR__) . '/config/bootstrap.php';
