e6b6685a4acd0578f4e815228cbf6133874d0f04
Alexandr Bubeshko <a.bubeshko@gmail.com>
2022-05-21 17:05:35 +0300

    build: add symfony/doctrine package to project

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -1,7 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
