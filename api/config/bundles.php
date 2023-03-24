4ef41e24d19c3416513ab24729b28f90d5e5025b
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-03-24 13:59:17 +0300

    chore: add requires documents to the nelmio/api-doc-bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
