08a9ae957bed2158d931d1d529e8cd03dfc8db67
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-03-24 14:29:21 +0300

    docs: add documentation for all categories route

--- api/src/UI/Http/Rest/Controller/Category/AllController.php
+++ api/src/UI/Http/Rest/Controller/Category/AllController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace UI\Http\Rest\Controller\Category;
 
 use App\Shared\Infrastructure\Persistence\ReadModel\Category\GetAllCategories;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
@@ -13,7 +14,13 @@ use UI\Http\Rest\Response\OpenApi;
 
 final class AllController
 {
-    #[Route('/categories', name: 'all_categories')]
+    #[Route('/categories', name: 'all_categories', methods: ['GET'])]
+    #[OA\Response(
+        ref: "#/components/responses/categories",
+        response: 200,
+        description: 'Returns all categories'
+    )]
+    #[OA\Tag(name: 'categories')]
     public function __invoke(GetAllCategories $repository, SerializerInterface $serializer): OpenApi
     {
         $output = $serializer->serialize($repository->all(), JsonEncoder::FORMAT, [
