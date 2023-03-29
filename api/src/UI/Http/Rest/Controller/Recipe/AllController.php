36da2a39101e3f44a3ff0739aa93ee2779d55ec2
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-03-29 13:18:42 +0300

    docs: add docs to all_recipes

--- api/src/UI/Http/Rest/Controller/Recipe/AllController.php
+++ api/src/UI/Http/Rest/Controller/Recipe/AllController.php
@@ -8,6 +8,8 @@ use App\Game\Domain\Repository\Recipe\FindAll;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use UI\Http\Rest\Response\OpenApi;
+use OpenApi\Attributes as OA;
+
 
 final class AllController
 {
@@ -15,6 +17,11 @@ final class AllController
      * @throws \Symfony\Component\Serializer\Exception\ExceptionInterface
      */
     #[Route('/recipes', name: 'all_recipes', methods: ['GET'])]
+    #[OA\Response(
+        ref: '#/components/responses/recipes',
+        response: 200,
+    )]
+    #[OA\Tag(name: 'recipes')]
     public function __invoke(FindAll $repository, NormalizerInterface $normalizer): OpenApi
     {
         $output = $normalizer->normalize($repository->all());
