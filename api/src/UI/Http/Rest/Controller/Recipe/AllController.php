caabcf8fab9f0f449591f4c7932c9fb461d99302
Alexandr Bubeshko <a.bubeshko@gmail.com>
2023-03-29 13:20:56 +0300

    docs: add docs to all_recipes

--- api/src/UI/Http/Rest/Controller/Recipe/AllController.php
+++ api/src/UI/Http/Rest/Controller/Recipe/AllController.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace UI\Http\Rest\Controller\Recipe;
 
 use App\Game\Domain\Repository\Recipe\FindAll;
+use OpenApi\Attributes as OA;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 use UI\Http\Rest\Response\OpenApi;
-use OpenApi\Attributes as OA;
-
 
 final class AllController
 {
